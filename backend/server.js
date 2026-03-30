58550e7528bba7a2e140964e6203e2a3bb0f837c
Aku <60570794+Aku-Hevo@users.noreply.github.com>
2026-03-30 22:06:06 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ app.get("/auth/discord", passport.authenticate("discord"));
 app.get("/auth/discord/callback",
   passport.authenticate("discord", { failureRedirect: "/" }),
   (req, res) => {
-    res.redirect("http://localhost:5173");
+    res.redirect("https://concessionario-fivem.vercel.app/");
   }
 );
 
